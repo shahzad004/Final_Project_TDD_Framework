a7e84af27b128ff7dbdd6726cb1824a93b4b6859
Shahzad Khan <75515143+shahzad004@users.noreply.github.com>
2025-02-18 21:07:10 -0800

    commit form intellij.

--- src/test/java/tests/JavaPractice.java
+++ src/test/java/tests/JavaPractice.java
@@ -2,7 +2,13 @@ package tests;
 
 public class JavaPractice {
 
+    public static void main(String[] args) {
 
+        Calculator cal=new Calculator();
+
+        System.out.println(cal.sum(100,30));
+
+    }
 
 
 
