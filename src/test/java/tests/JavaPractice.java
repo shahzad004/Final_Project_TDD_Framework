a327e9dfa245f0ec770ec55e222d1c036149498d
Shahzad Khan <75515143+shahzad004@users.noreply.github.com>
2025-02-18 20:39:30 -0800

    pushed via github

--- src/test/java/tests/JavaPractice.java
+++ src/test/java/tests/JavaPractice.java
@@ -2,7 +2,26 @@ package tests;
 
 public class JavaPractice {
 
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
 
+        System.out.println( cal.sum(5,4));
+        System.out.println( cal.sum(10,20,30));
+
+        System.out.println(cal.sum(10.2,9,90));
+        System.out.println(cal.sum(12,32.0));
+
+
+
+
+
+
+
+
+
+
+
+    }
 
 
 
